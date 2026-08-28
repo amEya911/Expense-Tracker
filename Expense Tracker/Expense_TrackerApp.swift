27b9212aa113a5b5f3f1053315e2699e16f9293e
ameya2023 <ameyakulkarni2023@gmail.com>
2026-08-27 20:49:35 -0400

    Initial Commit

---
+++ Expense Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense Tracker
+//
+//  Created by Ameya Kulkarni on 27/08/26.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
